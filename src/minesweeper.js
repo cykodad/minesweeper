65bd75f295f3b56e77841a3505f685f9627ddde1
Mehmet Bulut <chiliconlatte@gmail.com>
2017-10-14 23:31:54 +0200

    Minesweeper stage 3

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,57 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+function getRandomInt(min, max) {
+  let minInt = Math.ceil(min);
+  let maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+}
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+
+  return board;
 };
 
-const board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
 
-printBoard(board);
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
-board[0][1] = '1';
-board[2][2] = 'B';
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let ri = getRandomInt(0, numberOfRows);
+    let ci = getRandomInt(0, numberOfColumns);
+    if (board[ri][ci] === null) {
+      board[ri][ci] = 'B';
+      numberOfBombsPlaced++;
+    }
+  }
 
-printBoard(board);
+  return board;
+};
+
+const printBoard = board => {
+  let prettyBoard = board.map(row => row.join(' | '));
+  prettyBoard = prettyBoard.join('\n');
+  console.log(prettyBoard);
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+console.log('Player Board:');
+printBoard(playerBoard);
+
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('\nBomb Board:');
+printBoard(bombBoard);
